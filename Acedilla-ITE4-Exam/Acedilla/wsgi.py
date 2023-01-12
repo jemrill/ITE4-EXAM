d5dedad7f335add64c0bd8896298fedcf6e5f33c
jemrill <87561314+jemrill@users.noreply.github.com>
2023-01-12 14:34:41 +0800

    Update wsgi.py

--- Acedilla-ITE4-Exam/Acedilla/wsgi.py
+++ Acedilla-ITE4-Exam/Acedilla/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rockpaperscissors project.
+WSGI config for Acedilla project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockpaperscissors.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Acedilla.settings')
 
 application = get_wsgi_application()
